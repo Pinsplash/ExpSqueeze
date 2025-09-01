5c9bb7f8f22ce22793a3a9fae1aab6f598304b07
Pinsplash <39359267+Pinsplash@users.noreply.github.com>
2025-09-01 18:31:52 -0500

    sapphire progress file

--- src_imgui/examples/example_win32_directx9/main.cpp
+++ src_imgui/examples/example_win32_directx9/main.cpp
@@ -3322,7 +3322,7 @@ static void RegisterGames()
 	RegisterGame("Crystal", "crystal", "gen2_exp.csv", "crystal.pro", 2, { 184, 349, 798, 798 });
 	//g3
 	RegisterGame("Ruby", "ruby", "gen3_exp.csv", "ruby.pro", 3, { 350, 434 });
-	RegisterGame("Sapphire", "sapphire", "gen3_exp.csv", "", 3, { 350, 434 });
+	RegisterGame("Sapphire", "sapphire", "gen3_exp.csv", "sapphire.pro", 3, { 350, 434 });
 	RegisterGame("Emerald", "emerald", "gen3_exp.csv", "", 3, { 350, 449 });
 	RegisterGame("FireRed", "firered", "gen3_exp.csv", "", 3, { 258, 572, 825, 825 });
 	RegisterGame("LeafGreen", "leafgreen", "gen3_exp.csv", "", 3, { 258, 572, 825, 825 });
